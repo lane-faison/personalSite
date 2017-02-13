b72618fe6796790fa14a82dba6d717bf7dab826b
lanefaison2 <faisonusmc@gmail.com>
2017-02-12 23:24:29 -0700

    about page added

---
+++ public/assets/scripts/contact.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.email-btn').on('click', function(event) {
+    event.preventDefault();
+
+    let email = "faisonusmc@gmail.com";
+    var subject = 'Reaching out!';
+    var emailBody = '';
+    window.location = 'mailto:' + email + '?subject=' + subject + '&body=' + emailBody;
+  });
+})
