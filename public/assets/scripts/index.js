929aced43a1da984479e36df035b7fb9b4d833ea
lanefaison2 <faisonusmc@gmail.com>
2017-02-23 00:22:08 -0700

    minor jQuery add

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 function showInfo() {
   var title = $(this).attr("title");
   var name = $(this).attr("name");
-  var $title = '<p class="tool-name">' + title + '</p>';
+  var $title = '<p class="tool-name">' + '< ' + title + ' >' + '</p>';
 
   if (name === "fav") $('.hovered-fav-tool').append($title);
   if (name === "eager") $('.hovered-eager-tool').append($title);
